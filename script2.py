3d4b094b6b97f38cbaa3aa9c208b619480cb0970
John Chae <johnchae1994@yahoo.com>
2023-11-17 16:08:28 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,47 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from rf import return_portfolios, optimal_portfolio
-import codecademylib3_seaborn
 import numpy as np
 
-# 10. Change the path variable
-path = 'stock_data2.csv'
-
-# Load the stock data
+# 2 assets added to script1's stock data (7 total)
+path='stock_data2.csv'
 stock_data = pd.read_csv(path)
 
-# Find the quarterly for each period
 selected=list(stock_data.columns[1:])
 returns_quarterly = stock_data[selected].pct_change()
-
-# Find the expected returns 
 expected_returns = returns_quarterly.mean()
-
-# Find the covariance 
 cov_quarterly = returns_quarterly.cov()
 
-# Find a set of random portfolios
 random_portfolios = return_portfolios(expected_returns, cov_quarterly) 
 
-# Plot the set of random portfolios
 random_portfolios.plot.scatter(x='Volatility', y='Returns', fontsize=12)
 
-# Calculate the set of portfolios on the EF
 weights, returns, risks = optimal_portfolio(returns_quarterly[1:])
 
-# Plot the set of portfolios on the EF
+random_portfolios.plot.scatter(x='Volatility', y='Returns', fontsize=12)
 plt.plot(risks, returns, 'y-o')
+
 plt.ylabel('Expected Returns',fontsize=14)
 plt.xlabel('Volatility (Std. Deviation)',fontsize=14)
 plt.title('Efficient Frontier', fontsize=24)
 
-
-# Compare the set of portfolios on the EF to the 
 single_asset_std=np.sqrt(np.diagonal(cov_quarterly))
 plt.scatter(single_asset_std,expected_returns,marker='X',color='red',s=200)
 
-
-# 11 & 12. Compare to original
+# Compare new dataset to original from script1
 weak_EF = pd.read_csv('weak_risks_returns.csv')
 plt.plot(weak_EF['Risks'], weak_EF['Returns'], 'g-o')
 
